e52a74ea6baa93e12b1e747ffbef42cd20e67f4e
ahugari <markahugari@gmail.com>
2025-10-04 21:03:35 +0200

    Add script to create state remote backend in Azure Storage

---
+++ az_cli_tf_state_backend_setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Configuration Variables
+RESOURCE_GROUP_NAME="rg-prod-in-cmu"
+# Generate a unique name for storage account
+STORAGE_ACCOUNT_NAME="tfsynapsestate$(openssl rand -hex 4)"
+CONTAINER_NAME="tfstate"
+LOCATION="jioindiawest"
+
+# Will enable this once creation rights are granted
+# echo "Creating Resource Group: $RESOURCE_GROUP_NAME in $LOCATION..."
+# # Create resource group for state storage
+# az group create --name $RESOURCE_GROUP_NAME --location $LOCATION
+
+echo "Creating Storage account: $STORAGE_ACCOUNT_NAME..."
+# Create storage account 
+# using LRS for cost and setting encryption services
+az storage account create \
+    --name $STORAGE_ACCOUNT_NAME \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --location $LOCATION \
+    --sku Standard_LRS \
+    --encryption-services blob 
+
+echo "Creating Storage container: $CONTAINER_NAME..."
+# Create storage containcer
+# retrieve storage account key 
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query "[0].value" -o tsv)
+
+az storage container create \
+    --name $CONTAINER_NAME \
+    --account_name $STORAGE_ACCOUNT_NAME \
+    --account_key $ACCOUNT_KEY
+
+echo "Setup Complete"
+echo "Use the following values in the the tofu init command or you backend.tfvars file"
+echo "Resource Group Name: $RESOURCE_GROUP_NAME"
+echo "Storage account name: $STORAGE_ACCOUNT_NAME"
+echo "Container name: $CONTAINER_NAME"
+
